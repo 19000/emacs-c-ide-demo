b87b0bdcce5f372d21c2e143e766baf3d3dac092
19000 <19000.0rpm@gmail.com>
2017-11-08 01:50:40 +0800

    Add `query' param to `helm-do-ag'; So `helm-do-ag-project-root--symbol-at-point'

--- custom/setup-helm.el
+++ custom/setup-helm.el
@@ -72,22 +72,9 @@
           helm-locate-fuzzy-match t
           helm-display-header-line nil
 
-          ;;; XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-          ;; helm-fin-fdile ..xxx/
-          ;; helm-locate
-          ;; helm-find
           helm-locate-recursive-dirs-command "mdfind -onlyin %s -name %s"
           helm-locate-fuzzy-match nil
           helm-locate-command "mdfind -name %s %s")
-    ;;; XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-    ;; helm-fin-fdile ..xxx/
-    ;; helm-locate
-    ;; helm-find
-    ;; (setq-default helm-locate-recursive-dirs-command "mdfind -onlyin %s -name %s")
-
-    ;; (setq helm-locate-fuzzy-match nil)
-    ;; (setq helm-locate-command "mdfind -name %s %s")
-    ;;; XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
     (add-to-list 'helm-sources-using-default-as-input 'helm-source-man-pages)
 
@@ -163,17 +150,6 @@
       ;; If nil, you can slightly boost invoke speed in exchange for text color
       (setq helm-swoop-speed-or-color t))
 
-    ;;; More, define
-    ;;; XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-    ;; helm-fin-fdile ..xxx/
-    ;; helm-locate
-    ;; helm-find
-    ;; (setq-default helm-locate-recursive-dirs-command "mdfind -onlyin %s -name %s")
-
-    ;; (setq helm-locate-fuzzy-match nil)
-    ;; (setq helm-locate-command "mdfind -name %s %s")
-    ;;; XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
 
     (helm-mode 1)
 
@@ -181,8 +157,69 @@
       :init
       (helm-projectile-on)
       (setq projectile-completion-system 'helm)
-      (setq projectile-indexing-method 'alien))))
+      (setq projectile-indexing-method 'alien))
 
-(use-package helm-ag)
+    (use-package helm-ag
+      ;; :init
+      :config
+
+      ;; Rewrite this function, add `query' param
+      (defun helm-do-ag--helm (&optional query)
+        (let ((search-dir (if (not (helm-ag--windows-p))
+                              helm-ag--default-directory
+                            (if (helm-do-ag--target-one-directory-p helm-ag--default-target)
+                                (car helm-ag--default-target)
+                              helm-ag--default-directory))))
+          (helm-attrset 'name (helm-ag--helm-header search-dir)
+                        helm-source-do-ag)
+          (helm :sources '(helm-source-do-ag) :buffer "*helm-ag*" :keymap helm-do-ag-map
+                :input (or query
+                           (helm-ag--marked-input t)
+                           (helm-ag--insert-thing-at-point helm-ag-insert-at-point))
+                :history 'helm-ag--helm-history)))
+
+      ;; Rewrite this function, add `query' param
+      (defun helm-do-ag (&optional basedir targets query)
+        (interactive)
+        (require 'helm-mode)
+        (helm-ag--init-state)
+        (let* ((helm-ag--default-directory (or basedir default-directory))
+               (helm-ag--default-target (cond (targets targets)
+                                              ((and (helm-ag--windows-p) basedir) (list basedir))
+                                              (t
+                                               (when (and (not basedir) (not helm-ag--buffer-search))
+                                                 (helm-read-file-name
+                                                  "Search in file(s): "
+                                                  :default default-directory
+                                                  :marked-candidates t :must-match t)))))
+               (helm-do-ag--extensions (when helm-ag--default-target
+                                         (helm-ag--do-ag-searched-extensions)))
+               (one-directory-p (helm-do-ag--target-one-directory-p
+                                 helm-ag--default-target)))
+          (helm-ag--set-do-ag-option)
+          (helm-ag--set-command-features)
+          (helm-ag--save-current-context)
+          (helm-attrset 'search-this-file
+                        (and (= (length helm-ag--default-target) 1)
+                             (not (file-directory-p (car helm-ag--default-target)))
+                             (car helm-ag--default-target))
+                        helm-source-do-ag)
+          (if (or (helm-ag--windows-p) (not one-directory-p)) ;; Path argument must be specified on Windows
+              (helm-do-ag--helm query)
+            (let* ((helm-ag--default-directory
+                    (file-name-as-directory (car helm-ag--default-target)))
+                   (helm-ag--default-target nil))
+              (helm-do-ag--helm query)))))
+
+      (defun helm-do-ag-project-root--symbol-at-point ()
+        (interactive)
+        (helm-do-ag (helm-ag--project-root) nil (thing-at-point 'symbol)))
+
+      :bind
+      (("C-c h d" . helm-do-ag-project-root--symbol-at-point)))
+    ))
+
+;; (use-package helm-ag)
+
 
 (provide 'setup-helm)
